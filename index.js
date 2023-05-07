7132402f401c205e9ea529b4d6271692ebb28218
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-05-07 21:05:55 +0100

    dark mode

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ const themeChangeIcon = document.getElementById("themeChangeIcon");
 
 themeChangeIcon.addEventListener("click", function(){
     document.body.classList.toggle("dark-theme")
+
+    if(document.body.classList.contains("dark-theme")){
+        themeChangeIcon.classList.remove("fa-moon")
+        themeChangeIcon.classList.add("fa-sun")
+    } else{
+        themeChangeIcon.classList.remove("fa-sun")
+        themeChangeIcon.classList.add("fa-moon")
+    }
 })
